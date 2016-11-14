2807c70945b7956d60a7df9d1e388db1d70ee74b
Luke <sharlock@gmail.com>
2016-11-15 08:20:11 +1100

    refactor code

--- src/main/java/luke/zhou/model/Command.java
+++ src/main/java/luke/zhou/model/Command.java
@@ -1,5 +1,8 @@
 package luke.zhou.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created with IntelliJ IDEA.
  * User: LZhou
@@ -8,12 +11,51 @@ package luke.zhou.model;
  */
 public enum Command
 {
-    SEND_ALARM("send alarm"),
-    RAID("raid");
+    SEND_ALARM("send alarm","not available for user"),
+    RAID("raid", "check all in the raid list, raid once"),
+    REPEAT_RAID("repeat raid", "check all in the raid list, raid until stop"),
+    STOP_RAID("stop raid", "stop repeat raid"),
+    GET_INFO("info", "display brief information of account"),
+    HELP("help", "display command list");
 
     private final String value;
-    Command(String value)
+    private final String description;
+
+    Command(String value, String description)
     {
         this.value = value;
+        this.description = description;
+    }
+
+
+    public String getValue()
+    {
+        return value;
+    }
+
+    private static final Map<String, Command> lookup = new HashMap<>();
+
+    static
+    {
+        for (Command cmd : Command.values())
+        {
+            lookup.put(cmd.getValue(), cmd);
+        }
+    }
+
+    public static Command get(String value)
+    {
+        return lookup.get(value);
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%-20s", value) + description ;
     }
 }
